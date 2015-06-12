d478a527dc81f60c4ba04b41ec7b911f3f630e79
Matthew Baggett <matthew@baggett.me>
2015-06-12 14:42:45 +0100

    initial api code

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <?php
 require_once("bootstrap.php");
-$app = new \Slim\Slim();
-$app->get('/hello/:name', function ($name) {
-  echo "Hello, $name";
+use Slim\Slim;
+use Thru\ActiveRecord\ActiveRecord;
+$app = new Slim();
+
+// List function
+$app->get('/:model', function ($model) use ($app) {
+  $modelName = "\\Thru\\BankApi\\Models\\" . $model;
+  if(class_exists($modelName)){
+    /** @var ActiveRecord $o */
+    $o = new $modelName;
+    $list = $o->search()->exec();
+    $responseObjects = [];
+    foreach($list as $item){
+      /** @var ActiveRecord $item  */
+      $responseObjects = $item->__toPublicArray();
+    }
+
+    $response = $app->response();
+    $response['Content-Type'] = 'application/json';
+    $response->status(200);
+    $response->body(json_encode($responseObjects));
+
+  }else{
+    $app->notFound();
+  }
 });
+
+// fetch function
+$app->get('/:model/:id', function ($model, $id) {
+
+});
+
+
 $app->run();
