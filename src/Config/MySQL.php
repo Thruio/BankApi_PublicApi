99fd3e2003d8b50149e2695e42125e6981b5c978
Matthew Baggett <matthew@baggett.me>
2015-07-28 11:04:00 +0000

    Updated dependencies and config

--- src/Config/MySQL.php
+++ src/Config/MySQL.php
@@ -1,15 +1,16 @@
 <?php
-$settings = array_merge($_ENV, $_SERVER);
 // Database Settings
-if (isset($settings['THRUIO_ENV_MYSQL_DATABASE'])) {
-  $database = new \Thru\ActiveRecord\DatabaseLayer(array(
+if (isset($_ENV['DB_PORT'])) {
+    $host = parse_url($_ENV['DB_PORT']);
+
+    $database = new \Thru\ActiveRecord\DatabaseLayer(array(
     'db_type'     => 'Mysql',
-    'db_hostname' => $settings['MYSQL_1_ENV_TUTUM_NODE_FQDN'],
-    'db_port'     => $settings['MYSQL_1_PORT_3306_TCP_PORT'],
-    'db_username' => $settings['THRUIO_ENV_MYSQL_USER'],
-    'db_password' => $settings['THRUIO_ENV_MYSQL_PASS'],
-    'db_database' => $settings['THRUIO_ENV_MYSQL_DATABASE'],
-  ));
+    'db_hostname' => isset($host['hostname'])?$host['hostname']:$host['host'],
+    'db_port'     => $host['port'],
+    'db_username' => $_ENV['THRUIO_ENV_MYSQL_USER'],
+    'db_password' => $_ENV['THRUIO_ENV_MYSQL_PASS'],
+    'db_database' => $_ENV['THRUIO_ENV_MYSQL_DATABASE'],
+    ));
 } else {
   $database = new \Thru\ActiveRecord\DatabaseLayer(array(
     'db_type'     => 'Mysql',
