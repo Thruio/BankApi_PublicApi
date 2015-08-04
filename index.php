54b11d4c3308cca83c525871278e86af321d9693
Matthew Baggett <matthew@baggett.me>
2015-08-04 14:31:47 +0100

    Fix summary output

--- index.php
+++ index.php
@@ -8,6 +8,33 @@ use Thru\BankApi\Models\Account;
 
 $app = new Slim();
 
+$app->get("/summary/:account_holder_name", function($account_holder_name) use ($app){
+  $responseObjects = new StdClass();
+
+  /** @var AccountHolder $account_holder */
+  $account_holder = AccountHolder::search()->where('name', $account_holder_name)->execOne();
+  if(!$account_holder){
+    $app->notFound();
+  }
+
+  // Calculate Totals.
+  $responseObjects->total = 0;
+  $responseObjects->balances = [];
+  foreach($account_holder->getAccounts() as $account){
+    $nameHash = substr(md5($account->name),0,7);
+    $nameCleaned = trim(preg_replace("/\\([^)]+\\)/","",$account->name));
+    $displayName = "({$nameHash}) {$nameCleaned}";
+
+    $responseObjects->total += $account->getBalance()->value;
+    $responseObjects->balances[$displayName] = $account->getBalance()->value;
+  }
+
+  $response = $app->response();
+  $response['Content-Type'] = 'application/json';
+  $response->status(200);
+  $response->body(JsonPrettyPrinter::json($responseObjects));
+});
+
 // List function
 $app->get('/:model', function ($model) use ($app) {
   $modelName = "\\Thru\\BankApi\\Models\\" . $model;
@@ -34,27 +61,6 @@ $app->get('/:model', function ($model) use ($app) {
 // fetch function
 $app->get('/:model/:id', function ($model, $id) {
 
-});
-
-$app->get("/summary/:account_holder_name", function($account_holder_name) use ($app){
-  $responseObjects = new \StdClass();
-
-  /** @var AccountHolder $account_holder */
-  $account_holder = AccountHolder::search()->where('name', $account_holder_name)->execOne();
-  if(!$account_holder){
-    $app->notFound();
-  }
-
-  // Calculate Totals.
-  $responseObjects->total = 0;
-  foreach($account_holder->getAccounts() as $account){
-    $responseObjects->total += $account->getBalance()->value;
-  }
-
-  $response = $app->response();
-  $response['Content-Type'] = 'application/json';
-  $response->status(200);
-  $response->body(JsonPrettyPrinter::json($responseObjects));
 });
 
 $app->get("/", function (){
