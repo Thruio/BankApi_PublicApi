c937fd3b5ddb9d57ab1540a16d4dd0242a515c1d
Matthew Baggett <matthew@baggett.me>
2015-08-04 14:17:49 +0100

    Adding /summary/NameHere functionality

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once("bootstrap.php");
 use Slim\Slim;
 use Thru\ActiveRecord\ActiveRecord;
 use \Thru\JsonPrettyPrinter\JsonPrettyPrinter;
+use Thru\BankApi\Models\AccountHolder;
+use Thru\BankApi\Models\Account;
 
 $app = new Slim();
 
@@ -34,9 +36,29 @@ $app->get('/:model/:id', function ($model, $id) {
 
 });
 
+$app->get("/summary/:account_holder_name", function($account_holder_name) use ($app){
+  $responseObjects = new \StdClass();
+
+  /** @var AccountHolder $account_holder */
+  $account_holder = AccountHolder::search()->where('name', $account_holder_name)->execOne();
+  if(!$account_holder){
+    $app->notFound();
+  }
+
+  // Calculate Totals.
+  $responseObjects->total = 0;
+  foreach($account_holder->getAccounts() as $account){
+    $responseObjects->total += $account->getBalance()->value;
+  }
+
+  $response = $app->response();
+  $response['Content-Type'] = 'application/json';
+  $response->status(200);
+  $response->body(JsonPrettyPrinter::json($responseObjects));
+});
+
 $app->get("/", function (){
   echo "Api documentation doesn't exist yet.";
 });
-
 
 $app->run();
