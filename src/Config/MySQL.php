21a64165c5832779493f9b93bc2ea5b93c11a927
Matthew Baggett <matthew@baggett.me>
2015-12-22 10:40:39 +0000

    Use provided details

--- src/Config/MySQL.php
+++ src/Config/MySQL.php
@@ -1,23 +1,25 @@
 <?php
+$environment = array_merge($_ENV, $_SERVER);
+
 // Database Settings
-if (isset($_ENV['DB_PORT'])) {
-    $host = parse_url($_ENV['DB_PORT']);
+if (isset($_SERVER['DB_PORT'])) {
+    $host = parse_url($environment['DB_PORT']);
 
     $database = new \Thru\ActiveRecord\DatabaseLayer(array(
-    'db_type'     => 'Mysql',
-    'db_hostname' => isset($host['hostname'])?$host['hostname']:$host['host'],
-    'db_port'     => $host['port'],
-    'db_username' => $_ENV['THRUIO_ENV_MYSQL_USER'],
-    'db_password' => $_ENV['THRUIO_ENV_MYSQL_PASS'],
-    'db_database' => $_ENV['THRUIO_ENV_MYSQL_DATABASE'],
+            'db_type'     => 'Mysql',
+            'db_hostname' => isset($host['hostname'])?$host['hostname']:$host['host'],
+            'db_port'     => $host['port'],
+            'db_username' => $environment['DB_ENV_MYSQL_USER'],
+            'db_password' => $environment['DB_ENV_MYSQL_PASSWORD'],
+            'db_database' => $environment['DB_ENV_MYSQL_DATABASE'],
     ));
 } else {
-  $database = new \Thru\ActiveRecord\DatabaseLayer(array(
-    'db_type'     => 'Mysql',
-    'db_hostname' => "localhost",
-    'db_port'     => 3306,
-    'db_username' => "bankingapp",
-    'db_password' => "2l429q6Zug96iVU",
-    'db_database' => "bankingapp",
-  ));
+    $database = new \Thru\ActiveRecord\DatabaseLayer(array(
+            'db_type'     => 'Mysql',
+            'db_hostname' => "localhost",
+            'db_port'     => 3306,
+            'db_username' => "bankingapp",
+            'db_password' => "2l429q6Zug96iVU",
+            'db_database' => "bankingapp",
+    ));
 }
